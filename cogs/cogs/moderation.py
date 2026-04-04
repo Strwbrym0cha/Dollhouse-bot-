a1453863c11142069b4bc9f46a3726893ef0bb2c
Strwbrym0cha <Byoungharris@gmail.com>
2026-04-04 12:34:03 -0500

    Create moderation.py

---
+++ cogs/cogs/moderation.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int):
+        await ctx.channel.purge(limit=amount + 1)
+
+    @commands.command()
+    async def kick(self, ctx, member, *, reason=None):
+        await member.kick(reason=reason)
+
+    @commands.command()
+    async def ban(self, ctx, member, *, reason=None):
+        await member.ban(reason=reason)
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
